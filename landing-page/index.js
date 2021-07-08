b258078403a2f360b01e4da5435670e7597b61cb
Caíque Souto Lima <caiquesouto@hotmail.com>
2021-07-08 16:07:09 -0300

    Corrigido animação menu hamburger

--- landing-page/index.js
+++ landing-page/index.js
@@ -1,11 +1,11 @@
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".menu");
+const navMenu = document.querySelector("#menu");
 
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
     hamburger.classList.toggle("active");
-    menu.classList.toggle("active");
+    navMenu.classList.toggle("active");
 }
 
 const navLink = document.querySelectorAll("#menu a");
@@ -14,5 +14,5 @@ navLink.forEach(n => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
     hamburger.classList.remove("active");
-    menu.classList.remove("active");
+    navMenu.classList.remove("active");
 }
